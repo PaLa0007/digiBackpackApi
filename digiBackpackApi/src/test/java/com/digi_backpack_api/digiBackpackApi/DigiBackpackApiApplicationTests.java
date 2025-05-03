fc59004174def7a4abb3972e3e377e62e83bf9e6
Велизар Василев <stu2001261058@uni-plovdiv.bg>
2025-05-03 13:56:36 +0300

    Initial commit

---
+++ digiBackpackApi/src/test/java/com/digi_backpack_api/digiBackpackApi/DigiBackpackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digi_backpack_api.digiBackpackApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiBackpackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
